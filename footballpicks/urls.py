a8b38495c4692ed74fb9dd1db88398b35db4390b
Brennan Basinger <brennan.d.basinger@gmail.com>
2025-01-22 15:35:06 -0600

    Added the account and picks URLS in main urls.py

--- footballpicks/urls.py
+++ footballpicks/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+# mywebsite/urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from accounts.views import home_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', home_view, name='home'),
+
+    path('accounts/', include('accounts.urls')),  # We'll define in accounts/urls.py
+
+    path('picks/', include('picks.urls')),  # We'll define in picks/urls.py
 ]
