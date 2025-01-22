781b21c381741a09a5b4f92c30746b1c55e635fa
Brennan Basinger <brennan.d.basinger@gmail.com>
2025-01-22 15:32:51 -0600

    Created a view for picks

--- picks/views.py
+++ picks/views.py
@@ -1,3 +1,7 @@
+# picks/views.py
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def picks_view(request):
+    return render(request, 'picks.html')
