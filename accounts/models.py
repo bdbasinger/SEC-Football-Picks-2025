3169b1a33abf5b25d2cb1a2b4106ada47d6e1d3f
Brennan Basinger <brennan.d.basinger@gmail.com>
2025-01-22 15:22:19 -0600

    Added CustomUser model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,24 @@
+# accounts/models.py
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from .managers import CustomUserManager
 
-# Create your models here.
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    full_name = models.CharField(max_length=150)
+    phone_number = models.CharField(max_length=20)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'  # We use email as username
+    REQUIRED_FIELDS = ['full_name', 'phone_number']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+
