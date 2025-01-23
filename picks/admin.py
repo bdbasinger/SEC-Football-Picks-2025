9089ecf66554c2d1d6fb9c237834f287c4f8d7eb
Brennan Basinger <brennan.d.basinger@gmail.com>
2025-01-22 18:38:34 -0600

    Added picks model and ran migrations

--- picks/admin.py
+++ picks/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Pick
 
-# Register your models here.
+@admin.register(Pick)
+class PickAdmin(admin.ModelAdmin):
+    list_display = ('user', 'game', 'picked_winner')
--- picks/models.py
+++ picks/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
+from games.models import Game
 
-# Create your models here.
+class Pick(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='picks'
+    )
+    game = models.ForeignKey(
+        Game,
+        on_delete=models.CASCADE,
+        related_name='picks'
+    )
+    picked_winner = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.user} -> {self.picked_winner} for {self.game}"
