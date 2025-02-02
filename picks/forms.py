775cec766e24f590f236e3036bae5b92df7bda21
Brennan Basinger <brennan.d.basinger@gmail.com>
2025-02-01 19:34:28 -0600

    Reconfigured dynamically naming the team name as opposed to displaying HOME and AWAY on the PickForm

--- picks/forms.py
+++ picks/forms.py
@@ -9,9 +9,15 @@ class PickForm(forms.ModelForm):
         fields = ['id','outcome']
         widgets = {'id': forms.HiddenInput()}
 
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            print("Form instance ID:", self.instance.pk)  # Debugging line
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance and self.instance.game_id:  # Ensure game exists
+            game = self.instance.game
+            self.fields['outcome'].choices = [
+                ('HOME', game.home_team),  # Shows home team name
+                ('AWAY', game.away_team),  # Shows away team name
+            ]
+        print("Form instance ID:", self.instance.pk)  # Debugging line
 
 
 PickFormSet = modelformset_factory(
